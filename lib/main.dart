cbf83ca5a962d38b3a2667f51481ea3bdb98c12d
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-15 00:17:41 -0400

    style(main.dart): Retirando linhas de codigo e comentatios

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: null);
   }
 }
-
